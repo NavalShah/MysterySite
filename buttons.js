f4e83ca4ffc3e3fff2c4a057b2ee38d9599dcb3f
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2023-12-28 14:24:19 -0500

    Finished, now it opens a new tab to a random website
    Now we have to add more websites to the lists to make it truly random and unpredictable

--- buttons.js
+++ buttons.js
@@ -1,15 +1,24 @@
-function gameSite(){
+let games = document.querySelector("#games");
+let education = document.querySelector("#education");
+let wiki = document.querySelector("#wiki");
+
+games.addEventListener("click", function (){
     var gameList = ["https://www.coolmathgames.com/","https://itch.io/games/free/tag-pico-8 ","https://www.lexaloffle.com/pico-8.php"];
-    location.href = "http://" + gameList[Math.floor (Math.random()*gameList.length)];
-    return location.href;
-}
-function wikipediaSite(){
+    var random = gameList[Math.floor(Math.random() * gameList.length)];
+    window.open(random);
+});
+
+wiki.addEventListener("click", function (){
     var wikipediaList = ["https://en.wikipedia.org/wiki/Elon_Musk","https://en.wikipedia.org/wiki/Harry_Potter","https://en.wikipedia.org/wiki/NASA","https://en.wikipedia.org/wiki/Minecraft","https://en.wikipedia.org/wiki/Lamborghini"];
-    location.href = "http://" + wikipediaList[Math.floor (Math.random()*wikipediaList.length)];
-    return location.href;
-}
-function educationSite(){
+    var random = wikipediaList[Math.floor(Math.random() * wikipediaList.length)];
+    window.open(random);
+});
+
+education.addEventListener("click", function (){
     var educationList = ["https://www.ixl.com/","https://www.khanacademy.org/","https://quizlet.com","https://kids.nationalgeographic.com/","https://www.coolmathgames.com/"];
-    location.href = "http://" + educationList[Math.floor (Math.random()*educationList.length)];
-    return location.href;
-}
+    var random = educationList[Math.floor(Math.random() * educationList.length)];
+    window.open(random);
+});
+
+
+
